64bd2e8f9c47e1d9e98841dac5696a777aa1a486
tangjiapeng <msjptang@mail.scut.edu.cn>
2023-10-08 17:17:39 +0200

    fix bug of generation script

--- run/generate.sh
+++ run/generate.sh
@@ -4,4 +4,4 @@ python  generate_diffusion.py ../config/diffusion/diffusion_bedrooms_dim512_noma
  /Your experiment directory/diffusion_bedrooms_dim512_nomask_instancond_objfeats_lat32/gen_top2down_notexture_nofloor \
  /Your dataset directory/3d_front_processed/bedrooms/threed_future_model_bedroom.pkl   \
     --weight_file /Your experiment directory/diffusion_bedrooms_dim512_nomask_instancond_objfeats_lat32/model_38000  \
-    --without_screen  --n_sequences 1000 --render_top2down --save_mesh --no_texture --without_floor
+    --without_screen  --n_sequences 1000 --render_top2down --save_mesh --no_texture --without_floor  --clip_denoised --retrive_objfeats
